5aee1247ed54725d819cd67b41756ed6f1497c93
a23raslj <a23raslj@student.his.se>
2024-04-28 17:13:44 +0200

    Added an ArrayList to Mountain.java.

--- app/src/main/java/com/example/networking/Mountain.java
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -1,9 +1,17 @@
 package com.example.networking;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 @SuppressWarnings("WeakerAccess")
 public class Mountain {
 
     private String title;
+    ArrayList<Mountain> items = new ArrayList<>(Arrays.asList(
+            new Mountain("Matterhorn"),
+            new Mountain("Mont Blanc"),
+            new Mountain("Denali")
+    ));
 
     public Mountain(String title) {
         this.title = title;
