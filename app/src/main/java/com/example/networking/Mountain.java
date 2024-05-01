ac9c7b24cc56ae853ec94493f3d44f2ce98a30c6
a23raslj <a23raslj@student.his.se>
2024-05-01 13:50:19 +0200

    Created a nested static class inside Mountain.java.

--- app/src/main/java/com/example/networking/Mountain.java
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -10,4 +10,14 @@ public class Mountain {
     private int size;
     private int cost;
     private AuxData auxdata;
+
+    public static class AuxData {
+        private String wiki;
+        private String img;
+
+        public String getWiki() { return wiki; }
+        public void setWiki(String wiki) { this.wiki = wiki; }
+        public String getImg() { return img; }
+        public void setImg(String img) { this.img = img; }
+    }
 }
